27c21157b444dc8e2134faf467843acd2389d669
Tonny1997 <160511968+Tonny1997@users.noreply.github.com>
2024-03-07 12:36:58 +0300

    Create list_creation.py
    mylist=[]
    mylist.extend([10, 20, 30, 40])
    mylist.insert(1,15)
    mylist.extend([50, 60, 70])
    mylist.pop()
    mylist.sort()
    print()
    x = mylist.index(30)
    print('x:', x)
    print(mylist)

---
+++ list_creation.py
@@ -0,0 +1,10 @@
+mylist=[]
+mylist.extend([10, 20, 30, 40])
+mylist.insert(1,15)
+mylist.extend([50, 60, 70])
+mylist.pop()
+mylist.sort()
+print()
+x = mylist.index(30)
+print('x:', x)
+print(mylist)
